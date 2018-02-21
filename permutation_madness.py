6296c292d9488fbcd70e463761f02d8ad31960a1
Blair Sharpe <blair.a.sharpe@gmail.com>
2018-02-21 11:22:30 -0500

    Able to spin and exchange

--- permutation_madness.py
+++ permutation_madness.py
@@ -1,4 +1,48 @@
-import LinkedList
+
+def spin(move, programs):
+    """Spin moves N amount of programs to the front keeping their order
+
+    Args:
+        move (str): The amount of programs to move
+    programs (str): Programs in their current order
+
+    Returns:
+        str: Returns programs N amounts of programs spinned
+    """
+    program_index = int(move[1:])
+
+    # Find N amount of programs from the right
+    split_programs = programs[-program_index:] + programs[:-program_index]
+
+    return split_programs
+
+def exchange(move, programs):
+    """Exchange causes two programs to swap depending on two numbers given
+
+    Args:
+        move (str): Contains information on which two programs to swap
+    programs (str): Programs in their current order
+    """
+    program_one, program_two = tuple(move[1:].split("/"))
+    print(program_one, program_two)
+    programs = list(programs)
+    programs[int(program_two)], programs[int(program_one)] = \
+            programs[int(program_one)], programs[int(program_two)]
+
+    return "".join(programs)
+
+def partner(move, programs):
+    """Example function with types documented in the docstring.
+    Args:
+        move (str): The amount of programs to move
+    programs (str): Programs in their current order
+    """
+    program_one, program_two = tuple(move[1:].split("/"))
+
+    print(program_one, program_two)
+    print("partner")
+
+    return True
 
 
 if __name__ == "__main__":
@@ -16,11 +60,13 @@ if __name__ == "__main__":
         print("\n{}. {}".format(step, move))
 
         if move[0] == 'p':
-            partner(move)
+            partner(move, programs)
 
         elif move[0] == 's':
-            spin(move)
+            programs = spin(move, programs)
+            print(programs)
 
         else:
-            exchange(move)
+            programs = exchange(move, programs)
+            print(programs)
 
